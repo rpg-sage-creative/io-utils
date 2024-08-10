88e1b437f24224f74df606876253caa1a80694ef
Randal <randal.t.meyer@gmail.com>
2024-08-10 15:20:03 -0700

    text transmuter and get

--- test/pdf/pdf.mjs
+++ test/pdf/pdf.mjs
@@ -1,14 +1,14 @@
 import { assert, error, runTests } from "@rsc-utils/core-utils";
-import { PdfCacher, PdfJsonFieldManager } from "../../build/index.js";
+import { PdfCacher, PdfJsonFieldManager, readFileSync, readJsonFileSync, writeFileSync } from "../../build/index.js";
 
 runTests(async function test_PdfUtils() {
-	const url = "https://pf2.rpgsage.io/pathbuilder-2e-mal-level-4.pdf";
-	const content = await PdfCacher.read(url).catch(error);
-	assert(!!content, "File was NOT cached and read.");
-	const fields = PdfJsonFieldManager.from(content);
-	const name = fields.getValue("CharacterName");
-	assert(name === "Malken'throp (Mal)", `Wrong CharacterName: ${name}`);
-	// writeFileSync("./test/mal.json", content);
+	const urlOne = "https://pf2.rpgsage.io/pathbuilder-2e-mal-level-4.pdf";
+	const contentOne = await PdfCacher.read(urlOne).catch(error);
+	assert(!!contentOne, "File (Mal) was NOT cached and read.");
+	const managerOne = PdfJsonFieldManager.from(contentOne);
+	const nameOne = managerOne.getValue("CharacterName");
+	assert(nameOne === "Malken'throp (Mal)", `Wrong CharacterName: ${nameOne}`);
+	// writeFileSync("./test/pdf/out/mal.json", contentOne);
 
 	const urlTwo = `file://Users/randaltmeyer/git/rsc/io-utils/test/pdf/in/1264091676897448063.pdf`;
 	const managerTwo = await PdfCacher.createManager(urlTwo).catch(error);
@@ -16,5 +16,12 @@ runTests(async function test_PdfUtils() {
 	const ale_1 = managerTwo.isChecked("Ale_1");
 	assert(nameTwo === "Todd Campbell", `Wrong Character_Name: ${nameTwo}`);
 	assert(ale_1, `Not Checked (Ale_1): ${ale_1}`);
-	// writeFileSync("./test/todd.json", contentTwo);
+	// writeFileSync("./test/pdf/out/todd.json", contentTwo);
+
+	const urlThree = `file://Users/randaltmeyer/git/rsc/io-utils/test/pdf/in/BudMastercraft.pdf`;
+	const contentThree = await PdfCacher.read(urlThree).catch(error);
+	assert(!!contentThree, "File (Bud) was NOT cached and read.");
+	const managerThree = new PdfJsonFieldManager(contentThree, f => ({ id:+f.name.replace(/\D/g, ""), ...f }));
+	const nameThree = managerThree.getValue(1);
+	assert(nameThree === "Character Name", `Wrong Character_Name: ${nameThree}`);
 }, true);

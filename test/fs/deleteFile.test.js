6ce39366f79de48d1306175baf7e603a619ebed3
Randal <randal.t.meyer@gmail.com>
2025-05-05 14:10:19 -0700

    change filePath to to avoid sync/async collision

--- test/fs/deleteFile.test.js
+++ test/fs/deleteFile.test.js
@@ -1,31 +1,34 @@
+import { toLiteral } from "@rsc-utils/core-utils";
 import { deleteFile, writeFileSync } from "../../build/index.js";
 
 describe("fs", () => {
 	describe("deleteFile", () => {
 
+		const filePath = "made_up_path_async";
+
 		// default force is true, which bypasses errors deleting a non-existant file
-		test(`deleteFile("made_up_path") === true`, async () => {
-			expect(await deleteFile("made_up_path")).toBe(true);
+		test(`deleteFile(${toLiteral(filePath)}) === true`, async () => {
+			expect(await deleteFile(filePath)).toBe(true);
 		});
 		// passing in force causes it to throw
-		test(`deleteFile("made_up_path", { force:false }) !== true`, async () => {
-			expect(await deleteFile("made_up_path", { force:false }).catch(()=>{})).not.toBe(true);
+		test(`deleteFile(${toLiteral(filePath)}, { force:false }) !== true`, async () => {
+			expect(await deleteFile(filePath, { force:false }).catch(()=>{})).not.toBe(true);
 		});
 
-		test(`deleteFile("made_up_path", { checkExists:"before" }) === "NotFound"`, async () => {
-			expect(await deleteFile("made_up_path", { checkExists:"before" })).toBe("NotFound");
+		test(`deleteFile(${toLiteral(filePath)}, { checkExists:"before" }) === "NotFound"`, async () => {
+			expect(await deleteFile(filePath, { checkExists:"before" })).toBe("NotFound");
 		});
-		test(`deleteFile("made_up_path", { checkExists:"before", force:false }) === "NotFound"`, async () => {
-			expect(await deleteFile("made_up_path", { checkExists:"before", force:false })).toBe("NotFound");
-		});
-
-		test(`deleteFile("made_up_path", { checkExists:true }) === "NotFound"`, async () => {
-			expect(await deleteFile("made_up_path", { checkExists:true })).toBe("NotFound");
+		test(`deleteFile(${toLiteral(filePath)}, { checkExists:"before", force:false }) === "NotFound"`, async () => {
+			expect(await deleteFile(filePath, { checkExists:"before", force:false })).toBe("NotFound");
 		});
 
-		test(`deleteFile("made_up_path", { checkExists:true }) === "NotFound"`, async () => {
-			writeFileSync("made_up_path", "made_up_path");
-			expect(await deleteFile("made_up_path", { checkExists:true })).toBe(true);
+		test(`deleteFile(${toLiteral(filePath)}, { checkExists:true }) === "NotFound"`, async () => {
+			expect(await deleteFile(filePath, { checkExists:true })).toBe("NotFound");
+		});
+
+		test(`deleteFile(${toLiteral(filePath)}, { checkExists:true }) === "NotFound"`, async () => {
+			writeFileSync(filePath, filePath);
+			expect(await deleteFile(filePath, { checkExists:true })).toBe(true);
 		});
 
 	});
--- test/fs/deleteFileSync.test.js
+++ test/fs/deleteFileSync.test.js
@@ -1,24 +1,30 @@
+import { toLiteral } from "@rsc-utils/core-utils";
 import { deleteFileSync, writeFileSync } from "../../build/index.js";
 
 describe("fs", () => {
 	describe("deleteFileSync", () => {
 
-		test(`deleteFileSync("made_up_path") === true`, () => {
-			expect(deleteFileSync("made_up_path")).toBe(true);
-		});
-		test(`deleteFileSync("made_up_path", { force:false }) === true`, () => {
-			expect(() => deleteFileSync("made_up_path", { force:false })).toThrow();
-		});
-		test(`deleteFileSync("made_up_path", { checkExists:"before" }) === "NotFound"`, () => {
-			expect(deleteFileSync("made_up_path", { checkExists:"before" })).toBe("NotFound");
-		});
-		test(`deleteFileSync("made_up_path", { checkExists:true }) === "NotFound"`, () => {
-			expect(deleteFileSync("made_up_path", { checkExists:true })).toBe("NotFound");
+		const filePath = "made_up_path_sync";
+
+		test(`deleteFileSync(${toLiteral(filePath)}) === true`, () => {
+			expect(deleteFileSync(filePath)).toBe(true);
 		});
 
-		test(`deleteFileSync("made_up_path", { checkExists:true }) === "NotFound"`, () => {
-			writeFileSync("made_up_path", "made_up_path");
-			expect(deleteFileSync("made_up_path", { checkExists:true })).toBe(true);
+		test(`deleteFileSync(${toLiteral(filePath)}, { force:false }) === true`, () => {
+			expect(() => deleteFileSync(filePath, { force:false })).toThrow();
+		});
+
+		test(`deleteFileSync(${toLiteral(filePath)}, { checkExists:"before" }) === "NotFound"`, () => {
+			expect(deleteFileSync(filePath, { checkExists:"before" })).toBe("NotFound");
+		});
+
+		test(`deleteFileSync(${toLiteral(filePath)}, { checkExists:true }) === "NotFound"`, () => {
+			expect(deleteFileSync(filePath, { checkExists:true })).toBe("NotFound");
+		});
+
+		test(`deleteFileSync(${toLiteral(filePath)}, { checkExists:true }) === "NotFound"`, () => {
+			writeFileSync(filePath, filePath);
+			expect(deleteFileSync(filePath, { checkExists:true })).toBe(true);
 		});
 
 	});

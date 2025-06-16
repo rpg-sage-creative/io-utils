f0cc987e66d6c560b554ff837464669cab9be72d
Randal <randal.t.meyer@gmail.com>
2025-06-16 16:07:06 -0700

    update comment/signature

--- src/fs/internal/contentToFileOutput.ts
+++ src/fs/internal/contentToFileOutput.ts
@@ -3,7 +3,7 @@ import { stringifyJson } from "@rsc-utils/core-utils";
 /**
  * @internal
  * Ensures we have content that can be written to file.
- * Buffers and strings are passed on, an Object is converted using stringify or formattedStringify.
+ * Buffers and strings are passed on, an Object is converted using stringify.
  */
 export function contentToFileOutput<T>(content: T, formatted?: boolean): string | Buffer {
 	if (Buffer.isBuffer(content)) {

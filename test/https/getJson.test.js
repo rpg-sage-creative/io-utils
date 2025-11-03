c891379311dce957cf8d8870a12d7330c5299a3b
Randal <randal.t.meyer@gmail.com>
2025-11-03 13:09:20 -0800

    add test launch script; support gzip in getbuffer

--- test/https/getJson.test.js
+++ test/https/getJson.test.js
@@ -1,4 +1,4 @@
-import { toLiteral } from "@rsc-utils/core-utils";
+import { tagLiterals } from "@rsc-utils/core-utils";
 import { getJson } from "../../build/index.js";
 
 describe("https", () => {
@@ -6,10 +6,20 @@ describe("https", () => {
 
 		const url = "https://pf2.rpgsage.io/abc/some.json";
 
-		test(`getJson(${toLiteral(url)})`, async () => {
+		test(tagLiterals`getJson(${url})`, async () => {
 			const json = await getJson(url);
 			expect(Array.isArray(json)).toBe(true);
 		});
 
+		const urlTwo = "https://hephaistos.online/query";
+		const dataTwo = {"query":`{\n\tcharacters(readOnlyPermalinkId: "213430835") {\n\t\tjson\n\t}\n}`};
+		test(tagLiterals`getJson(${urlTwo}, ${dataTwo})`, async () => {
+			const graphQlJson = await getJson(urlTwo, dataTwo);
+			expect(graphQlJson).toBeDefined();
+			const { data:{ characters:{ 0:{ json:raw } } } } = graphQlJson;
+			// console.log({raw});
+			const json = JSON.parse(raw);
+			// console.log({json});
+		});
 	});
 });
